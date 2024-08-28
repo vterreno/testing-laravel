fc4597be1db6f0ab93c7c7f08b2baa1af0c599f1
Valentino Terreno <ninot2016@gmail.com>
2024-08-28 18:29:35 -0300

    Modificación de webpack.mix,js para hot reload automatico

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,3 +14,17 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .sourceMaps();
+    
+
+mix.browserSync({
+    proxy: 'http://127.0.0.1:8000/',
+    files: [
+        'resources/views/**/*.php',
+        'resources/js/**/*.js',
+        'resources/sass/**/*.scss',
+        'resources/**/*.vue', 
+        'public/js/**/*.js',
+        'public/css/**/*.css'
+    ],
+    notify: false    
+});
